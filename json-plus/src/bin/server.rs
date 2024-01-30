81b94d51fd21e1b2879df4d230190a28eb8e85e6
John Stanford <johnwstanford@gmail.com>
2024-01-30 00:25:23 -0600

    Setting up to multiplex to subscribers

--- json-plus/src/bin/server.rs
+++ json-plus/src/bin/server.rs
@@ -1,46 +1,106 @@
-use std::io::Write;
-use std::net::{Ipv4Addr, TcpListener};
-use std::time::Duration;
+use std::collections::VecDeque;
+use std::io::{Read, Write};
+use std::net::{TcpListener, TcpStream};
+use std::sync::{Arc, Mutex};
+use std::time::{Duration, Instant};
 
 const HEARTBEAT: [u8; 20] = [
     0x4A, 0x53, 0x4F, 0x4E, 0x50, 0x4C, 0x55, 0x53,     // "JSONPLUS"
-    0x00, 0x00, 0x00, 0x00,     // Overall length (including 20 bytes of header)
+    0x14, 0x00, 0x00, 0x00,     // Overall length (including 20 bytes of header)
     0x00, 0x00, 0x00, 0x00,     // JSON length
     0x00, 0x00, 0x00, 0x00,     // Payload length
 ];
 
 const HEARTBEAT_RATE: Duration = Duration::from_secs(10);
 
+#[derive(Debug)]
+struct Header {
+    title_word: [u8; 8],
+    total_len: u32,
+    json_len: u32,
+    payload_len: u32,
+}
+
 fn main() -> Result<(), &'static str> {
 
     println!("JSON Plus Server");
 
+    let in_port: u16  = 30008;
     let out_port: u16 = 30009;
 
-    let listener = TcpListener::bind(format!("0.0.0.0:{}", out_port))
+    let in_streams: Arc<Mutex<VecDeque<TcpStream>>> = Arc::new(Mutex::new(VecDeque::new()));
+    let out_streams: Arc<Mutex<VecDeque<TcpStream>>> = Arc::new(Mutex::new(VecDeque::new()));
+
+    // let transit: Arc<Mutex<VecDeque<Vec<u8>>>> = Arc::new(Mutex::new(VecDeque::new()));
+    // let recycle: Arc<Mutex<VecDeque<Vec<u8>>>> = Arc::new(Mutex::new(VecDeque::new()));
+
+    let in_streams_listen = in_streams.clone();
+    let in_listener = TcpListener::bind(format!("0.0.0.0:{}", in_port))
         .map_err(|_| "Unable to bind")?;
+    std::thread::spawn(move || {
 
-    for stream in listener.incoming() {
+        for stream in in_listener.incoming() {
+            if let Ok(stream) = stream {
+                let addr = stream.peer_addr();
+                println!("Connected to input from {:?}", addr);
+                in_streams_listen.lock().unwrap().push_back(stream);
+            }
+        }
+    });
 
-        if let Ok(mut stream) = stream {
-
-            let addr = stream.peer_addr();
-            println!("Connected to {:?}", addr);
-
-            std::thread::spawn(move || {
-
-                while let Ok(_) = stream.write_all(&HEARTBEAT) {
-                    std::thread::sleep(HEARTBEAT_RATE);
-                }
-
-                println!("Dropped connection to {:?}", addr);
-
-            });
-
+    let out_streams_listen = out_streams.clone();
+    let out_listener = TcpListener::bind(format!("0.0.0.0:{}", out_port))
+        .map_err(|_| "Unable to bind")?;
+    std::thread::spawn(move || {
+        for stream in out_listener.incoming() {
+            if let Ok(stream) = stream {
+                let addr = stream.peer_addr();
+                println!("New output subscription request from {:?}", addr);
+                out_streams_listen.lock().unwrap().push_back(stream);
+            }
         }
 
-    }
+    });
 
-    Ok(())
+    let in_streams_worker = in_streams.clone();
+    // let transit_worker = transit.clone();
+    // let recycle_worker = recycle.clone();
+    std::thread::spawn(move || {
+        let mut header_buff: [u8; 20] = [0; 20];
+        loop {
+            let opt_s = in_streams_worker.lock().unwrap().pop_front();
+            if let Some(mut s) = opt_s {
+                let r = s.read(&mut header_buff).ok();
+                if r == Some(20) {
+                    let header: Header = unsafe{ std::mem::transmute(header_buff.clone()) };
+                    let n = header.total_len as usize;
+                    println!("IN: {:?}", header);
+
+                    // let mut buff = recycle_worker.lock().unwrap().pop_front().unwrap_or_default();
+                    let mut buff: Vec<u8> = vec![];
+                    buff.resize(n, 0);
+                    if s.read(&mut buff[20..]).ok() == Some(n-20) {
+                        println!("Size OK");
+                    }
+                }
+
+                if r.is_none() {
+                    in_streams_worker.lock().unwrap().push_back(s);
+                }
+
+            }
+        }
+    });
+
+    // Use the main thread to process output
+    let mut last_heartbeat = Instant::now();
+    loop {
+        if last_heartbeat.elapsed() > HEARTBEAT_RATE {
+            last_heartbeat = Instant::now();
+
+            // This is also a chance to prune connections
+            out_streams.lock().unwrap().retain(|mut s| s.write_all(&HEARTBEAT).is_ok());
+        }
+    }
 
 }
